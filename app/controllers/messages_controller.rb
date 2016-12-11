6855e7d3a4ad4e03248d236bf71d01507a35766a
Haruki Sasaki <teampippi@gmail.com>
2016-12-11 09:12:42 +0000

    edit update destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     # Messageをすべて取得する。
     @messages = Message.all
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に西欧した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました。'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました。'
   end
   
   def create
@@ -20,5 +40,9 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
